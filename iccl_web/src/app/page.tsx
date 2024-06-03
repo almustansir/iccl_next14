1746b384a21426c5263e865ecf4bc1347275e681
Samit_08 <almustansir465@gmail.com>
2024-06-03 15:21:59 +0600

    Design adjustments

--- iccl_web/src/app/page.tsx
+++ iccl_web/src/app/page.tsx
@@ -1,10 +1,8 @@
 import AchivementsComp from "./components/AchivementsComp";
 import GetInTouchComp from "./components/GetInTouchComp";
 import HeroComp from "./components/HeroComp";
-import InfoCardComp from "./components/InfoCardComp";
 import MDcomp from "./components/MDcomp";
-import NewVideoPlayer from "./components/NewVideoPlayer";
-import PageHeaderComp from "./components/PageHeaderComp";
+
 import ServicesComp from "./components/ServicesComp";
 
 export default function Home() {
